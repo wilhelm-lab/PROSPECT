25052fc1307b9bb1455b84f02045ebc8eafd6fc1
omsh <omar.shouman@gmail.com>
2022-06-06 14:13:02 +0200

    Create __init__.py

---
+++ prospect-dataset/__init__.py
@@ -0,0 +1,10 @@
+__version__ = '0.0.1'
+
+META_DATA = {
+    "author": "Wassim Gabriel, Omar Shouman, Mathias Wilhelm",
+    "author_email": "wassim.gabriel@tum.de, omar.shouman@tum.de",
+    "description": "Proteomics Dataset for Machine Learning",
+    "package_name": "prospect-dataset",
+    "copyright_text": "2022, Wilhelm Lab, TU Munich.",
+    "github_url": "https://github.com/wilhelm-lab/PROSPECT",
+}
