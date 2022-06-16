e3a3d5928283d6e070e3c2b57e9cf5349eb8d190
omsh <omar.shouman@gmail.com>
2022-06-16 17:18:14 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     install_requires=[
         'pandas',
+        'fastparquet',
         'pyarrow',
         'requests',
         'bs4'
