ee4e27972a19577962e60b9e41a41220b655e47b
omsh <omar.shouman@gmail.com>
2022-07-17 20:51:40 +0200

    fixed init.py in metrics

--- prospectdataset/__init__.py
+++ prospectdataset/__init__.py
@@ -1,4 +1,4 @@
 from .download import download_dataset
-from .metrics import 
+from .metrics import timedelta_metric, masked_spectral_distance
 
 __all__ = [download_dataset, timedelta_metric, masked_spectral_distance]
