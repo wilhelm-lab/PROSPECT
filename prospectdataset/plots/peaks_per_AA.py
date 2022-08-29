1bbf2f36271a5aa95e98b595b5ddb1c7a66df02c
WassimG <wassim.gabriel@gmail.com>
2022-08-29 12:37:35 +0200

    added peptides charges distribution plot

--- prospectdataset/plots/peaks_per_AA.py
+++ prospectdataset/plots/peaks_per_AA.py
@@ -17,31 +17,32 @@ parser.add_argument(
     "--dir_path_metadata",
     help="Path to the directory with the meta data files"
 )
+
+
 def main(sys_args=sys.argv[1:]):
     args = parser.parse_args(sys_args)
     meta_data_list = []
     for j, parquet_path_md in enumerate(Path(args.dir_path_metadata).glob("*meta_data.parquet")):
-        file_name = str(parquet_path_md).split("/")[-1].replace("_meta_data.parquet", "")
-        path_ann = args.dir_path_annotation + '/' +  file_name
 
+        file_name = str(parquet_path_md).split("/")[-1].replace("_meta_data.parquet", "")
+        path_ann = args.dir_path_annotation + '/' + file_name
         meta_data = pd.read_parquet(parquet_path_md, columns=['raw_file', 'scan_number', 'peptide_length'],
-                                    engine="fastparquet")
+                                    engine="pyarrow")
         count = 0
+
         for f, parquet_path in enumerate(Path(path_ann).glob("*annotation.parquet")):
-            try:
-                annotation = pd.read_parquet(parquet_path, columns=['raw_file', 'scan_number', 'experimental_mass',
-                                                                    'ion_type', 'no', 'charge', 'neutral_loss'],
-                                             engine="pyarrow")
-                annotation.drop_duplicates(subset=['raw_file', 'scan_number', 'experimental_mass'], keep="first",
-                                           inplace=True)
-                annotation.drop_duplicates(subset=['raw_file', 'scan_number', 'ion_type', 'no', 'charge', 'neutral_loss'],
-                                           keep="first", inplace=True)
-                meta_data = meta_data.merge(annotation.groupby(['raw_file', 'scan_number']).size().to_frame().reset_index(),
-                                            left_on=['raw_file', 'scan_number'], right_on=['raw_file', 'scan_number'],
-                                            how="left")
-                count += 1
-            except:
-                continue
+
+            annotation = pd.read_parquet(parquet_path, columns=['raw_file', 'scan_number', 'experimental_mass',
+                                                                'ion_type', 'no', 'charge', 'neutral_loss'],
+                                         engine="pyarrow")
+            annotation.drop_duplicates(subset=['raw_file', 'scan_number', 'experimental_mass'], keep="first",
+                                       inplace=True)
+            annotation.drop_duplicates(subset=['raw_file', 'scan_number', 'ion_type', 'no', 'charge', 'neutral_loss'],
+                                       keep="first", inplace=True)
+            meta_data = meta_data.merge(annotation.groupby(['raw_file', 'scan_number']).size().to_frame().reset_index(),
+                                        left_on=['raw_file', 'scan_number'], right_on=['raw_file', 'scan_number'],
+                                        how="left")
+            count += 1
 
         meta_data["peaks"] = meta_data.iloc[:, -count:].sum(axis=1)
         meta_data_list.append(meta_data[['raw_file', 'scan_number', "peptide_length", "peaks"]])
@@ -49,7 +50,6 @@ def main(sys_args=sys.argv[1:]):
     concat_meta_data = pd.concat(meta_data_list, ignore_index=True)
     concat_meta_data.dropna(inplace=True)
     concat_meta_data.to_csv('meta_data_concat.csv')
-
     plt.rcParams['figure.figsize'] = [30, 15]
     concat_meta_data["peaks/length"] = concat_meta_data["peaks"] / concat_meta_data["peptide_length"]
     ax = sns.boxplot(y="peaks/length", x="peptide_length", data=concat_meta_data, color="seagreen", showfliers=False)
---
+++ prospectdataset/plots/peptides_charges_distribution.py
@@ -0,0 +1,36 @@
+import sys
+import argparse
+import seaborn as sns
+import matplotlib.pyplot as plt
+from pathlib import Path
+import pandas as pd
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "-d",
+    "--dir_path",
+    help="Path to the directory with the metadata files"
+)
+
+
+def main(sys_args=sys.argv[1:]):
+    args = parser.parse_args(sys_args)
+    pools = []
+    for i, parquet_path in enumerate(Path(args.dir_path).glob("*meta_data.parquet")):
+        pool_meta_data = pd.read_parquet(parquet_path)
+        pools.append(pool_meta_data)
+
+    meta_data = pd.concat(pools)
+    df_pie = meta_data.groupby(['precursor_charge', 'peptide_length']).size().unstack(fill_value=0)
+    colors = sns.color_palette()[0:6]
+
+    f, axes = plt.subplots(1,len(meta_data["peptide_length"].unique()), figsize=(20,5))
+    for ax, col in zip(axes, df_pie.columns):
+        patches, text = ax.pie(df_pie[col].values, colors=colors)
+        ax.set(title=col)
+
+    plt.legend(labels=['1', '2', '3', '4', '5', '6'], loc='center', ncol=6, bbox_to_anchor=(0.5, 2),
+              fancybox=True, shadow=True)
+    plt.axis('off')
+
+    plt.savefig('peptides_charges_dist.svg', dpi=300, bbox_inches='tight')
