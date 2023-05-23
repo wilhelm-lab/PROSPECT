7302d5bde8d476dc2ef0c603be666e4c8730a9df
omsh <omar.shouman@gmail.com>
2023-05-24 00:01:06 +0200

    Feature/intensity data prep (#1) (#2)
    * [WIP]: intensity prep
    
    * refactored intensity processing script
    
    * if conditions for safe retrieval of columns
    
    * added functions to __init__
    
    * fixed condition
    
    * testing cond
    
    * key conditions
    
    * set operation fixed + gitignore
    
    * full save filepath
    
    * minor fixes

--- prospectdataset/__init__.py
+++ prospectdataset/__init__.py
@@ -1,4 +1,5 @@
 from .download import download_dataset
-from .metrics import timedelta_metric, masked_spectral_distance
+from .metrics import masked_spectral_distance, timedelta_metric
+from .process_intensity_data import download_process_pool
 
-__all__ = [download_dataset, timedelta_metric, masked_spectral_distance]
+__all__ = [download_dataset, timedelta_metric, masked_spectral_distance, download_process_pool]
