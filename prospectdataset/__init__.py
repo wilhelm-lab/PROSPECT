8f5deeb620c2621f895fbf03d2408384b0eb2af9
omsh <omar.shouman@gmail.com>
2023-05-24 21:41:51 +0200

    export in __init__

--- prospectdataset/__init__.py
+++ prospectdataset/__init__.py
@@ -1,3 +1,5 @@
 from .download import download_dataset
+from .process_intensity_data import download_process_pool
 
-__all__ = [download_dataset]
+__all__ = ["download_dataset",
+           "download_process_pool"]
