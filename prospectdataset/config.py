b9e8aa272da6d6d8883c1e3ee68a79d45c00484d
Omar Shouman <omar.shouman@gmail.com>
2023-06-14 20:31:26 +0200

    Develop (#5)
    * [WIP] readme updates + download records
    
    * fix readme.md
    
    * updated readme.md
    
    * description in readme.md
    
    * readme updates
    
    * record names
    
    * updated record names
    
    * sequence filtering criteria
    
    * updated gitignore
    
    * Update version.py

--- prospectdataset/config.py
+++ prospectdataset/config.py
@@ -1,3 +1,12 @@
 ZENODO_BASE = 'https://zenodo.org/'
-ZENODO_RECORD = ZENODO_BASE + 'record/6602020'
-  
+ZENODO_BASE_RECORD = 'https://zenodo.org/record/'
+
+AVAILABLE_DATASET_RECORDS = {
+    'prospect': '6602020',
+    'tmt': '8003138',
+    'multi_ptm': '7998644',
+    'tmt_ptm': '8003152',
+    'test_ptm': '8003156'
+}
+
+AVAILABLE_DATASET_URLS = {k: ZENODO_BASE_RECORD + v for k, v in AVAILABLE_DATASET_RECORDS.items()}
